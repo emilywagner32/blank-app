8c3f135b38212385be4a7496b729ac257bb2a9b6
emilywagner32 <emwagner1919@gmail.com>
2025-05-24 22:06:55 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
-st.title("Jobsite Work flow")
+st.title("Daily Job Site Form")
 with st.form("LMP_form"):
    st.write("Jobsite End of Day Summary")
    jobsite = st.text_area('Job site location:')
    description = st.text_area('Description of work completed:', height=300)
    brk = st.selectbox('Was any Equiptment broken?', ['yes', 'no'])
+   brkn_desc = st.text_area('Describe what was broken:', height=300)
    st.form_submit_button('Submit')
