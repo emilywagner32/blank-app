8e27d5a256c56556725a41643a43b7027f4910f5
emilywagner32 <emwagner1919@gmail.com>
2025-05-24 21:59:34 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,5 @@ with st.form("LMP_form"):
    jobsite = st.text_area('Job site location:')
    description = st.text_area('Description of work completed:', height=300)
    equipt = st.selectbox('Were any tools or equiptment broken?' ['yes', 'no'])
+   description_broken = st.text_area('Describe what was broken:':', height=300)
    st.form_submit_button('Submit my picks')
