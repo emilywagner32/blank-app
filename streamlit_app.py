e377216266dfd579651368ad79366a105041a8b3
emilywagner32 <emwagner1919@gmail.com>
2025-05-24 21:55:44 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit as st
 st.title("Jobsite Work flow")
 with st.form("LMP_form"):
    st.write("Jobsite End of Day Summary")
-   jobsite = st.text_area('Job site location:', height=300)
-   my_color = st.selectbox('Pick a color', ['red','orange','green','blue','violet'])
+   jobsite = st.text_area('Job site location:', height=100)
+   description = st.text_area('Description of work completed:', height=300)
    st.form_submit_button('Submit my picks')
