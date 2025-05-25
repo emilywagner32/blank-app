64fda20324acf38dd10bdf36d93f2883d9d6e6a6
emilywagner32 <emwagner1919@gmail.com>
2025-05-24 21:29:56 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Jobsite Workflow")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
