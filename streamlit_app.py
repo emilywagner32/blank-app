36ac50975db3ece5f959707a4a5af5b1c914f319
emilywagner32 <emwagner1919@gmail.com>
2025-05-24 21:56:08 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit as st
 st.title("Jobsite Work flow")
 with st.form("LMP_form"):
    st.write("Jobsite End of Day Summary")
-   jobsite = st.text_area('Job site location:', height=100)
+   jobsite = st.text_area('Job site location:', height=68)
    description = st.text_area('Description of work completed:', height=300)
    st.form_submit_button('Submit my picks')
