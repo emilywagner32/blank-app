7fe584408a5504b576d45357019ba55d0c3cf6f3
emilywagner32 <emwagner1919@gmail.com>
2025-05-24 22:05:18 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ with st.form("LMP_form"):
    st.write("Jobsite End of Day Summary")
    jobsite = st.text_area('Job site location:')
    description = st.text_area('Description of work completed:', height=300)
-   brk = st.text_area('Was any Equiptment broken?')
+   brk = st.selectbox('Was any Equiptment broken?', ['yes', 'no'])
    st.form_submit_button('Submit')
