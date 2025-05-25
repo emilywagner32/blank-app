db9871279bf5ee8665dc0a5f3eb72071e98cdef7
emilywagner32 <emwagner1919@gmail.com>
2025-05-24 21:39:40 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("Jobsite Work flow")
-st.form("my_form"):
+with st.form("my_form"):
    st.write("Inside the form")
    my_number = st.slider('Pick a number', 1, 10)
    my_color = st.selectbox('Pick a color', ['red','orange','green','blue','violet'])
